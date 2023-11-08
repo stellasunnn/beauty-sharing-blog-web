4b1ddc8348d13dcb5fcc3ecf063c36e97df0b3a8
Zhuoranll <li.zhuoran@northeastern.edu>
2023-11-07 16:55:31 -0800

    ZL use console test several functions and statements.

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-console.log("Hello World!");
-
 const btn = document.querySelector(".btn-open");
 const form = document.querySelector(".fact-form");
 
@@ -13,3 +11,97 @@ btn.addEventListener("click", function () {
     btn.textContent = "Share a fact";
   }
 });
+
+/*
+let votesInteresting = 23;
+let votesMindlowing = 5;
+const text = "Lisbon is the capital of Portugal";
+
+votesInteresting = votesInteresting + 1;
+votesInteresting++;
+console.log(votesInteresting);
+
+let totalUpvotes = votesInteresting + votesMindlowing;
+console.log("Upvotes:", totalUpvotes);
+
+let votesFalse = 4;
+const isCorrect = votesFalse < totalUpvotes;
+console.log(isCorrect);
+
+console.log(parseInt("24.533ccc"));
+*/
+
+/*
+function calcFactAge(year) {
+  const currentYear = new Date().getFullYear();
+  const age = currentYear - year;
+
+  if (age >= 0) return age;
+  else return `Impossible year. Year needs to be less or equal ${currentYear}`;
+}
+
+const age1 = calcFactAge(2015);
+console.log(age1);
+console.log(calcFactAge(2020));
+console.log(calcFactAge(1990));
+console.log(calcFactAge(2037));
+
+const calcFactAge2 = (year) =>
+  year <= new Date().getFullYear()
+    ? new Date().getFullYear() - year
+    : `Impossible year. Year needs to be less or equal ${new Date().getFullYear()}`;
+
+console.log(calcFactAge2(2015));
+console.log(calcFactAge2(2037));
+*/
+
+/*
+let votesInteresting = 20;
+let votesMindlowing = 5;
+
+if (votesInteresting === votesMindlowing) {
+  alert("This fact is equally interesting and mindblowing");
+} else if (votesInteresting > votesMindlowing) {
+  console.log("Interesting fact!");
+} else if (votesInteresting < votesMindlowing) {
+  console.log("Mindblowing fact!");
+}
+
+if (votesMindlowing) {
+  console.log("MINDBLOWING FACT!");
+} else {
+  console.log("Not so special...");
+}
+
+let votesFalse = 7;
+const totalUpvotes = votesInteresting + votesMindlowing;
+
+const message =
+  totalUpvotes > votesFalse
+    ? "The fact is true"
+    : "Might be flase, check more sources...";
+// alert(message);
+
+const text = "Lisbon is the capital of Portugal";
+const upperText = text.toUpperCase();
+console.log(upperText);
+
+const str = `The current fact is "${text}". It is ${calcFactAge(
+  2015
+)} years old. It is probably ${
+  totalUpvotes > votesFalse ? "correct" : "not true"
+}.`;
+console.log(str);
+*/
+
+const fact = ["Lisbon is the captical of Portugal", 2015, true];
+console.log(fact);
+console.log(fact[2]);
+console.log(fact.length);
+console.log(fact[fact.length - 1]);
+
+const [text, createdIn] = fact;
+console.log(createdIn);
+
+const newFact = [...fact, "society"];
+console.log(newFact);
